73fcff7b88c0178b9b0b2160cc70a3883e7841c1
tiaak <istianaasalsabila15@gmail.com>
2023-12-03 14:55:32 +0700

    add maps fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -53,6 +54,7 @@ dependencies {
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("com.google.ar.sceneform:filament-android:1.17.1")
     implementation("androidx.datastore:datastore-core:1.0.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -60,6 +62,8 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     implementation("androidx.activity:activity-ktx:1.8.1")
+
+    implementation ("com.google.android.gms:play-services-maps:17.0.1")
 
 
 }
