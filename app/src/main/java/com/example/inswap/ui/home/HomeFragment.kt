7aaf5f08612a99b675c19a8261e7617e743c46f6
tiaak <istianaasalsabila15@gmail.com>
2023-12-05 02:20:41 +0700

    add search bar

--- app/src/main/java/com/example/inswap/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/inswap/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.inswap.databinding.FragmentHomeBinding
+import com.google.android.material.searchbar.SearchBar;
 
 class HomeFragment : Fragment() {
 
