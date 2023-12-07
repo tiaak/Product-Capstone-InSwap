21924758653e1c034a55822a991c43843d5b3711
tiaak <istianaasalsabila15@gmail.com>
2023-12-07 22:30:43 +0700

    make upload ingredient interface

--- app/src/main/java/com/example/inswap/ui/upload/UploadActivity.kt
+++ app/src/main/java/com/example/inswap/ui/upload/UploadActivity.kt
@@ -8,5 +8,6 @@ class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+        supportActionBar?.hide()
     }
 }
