8aef536b9bf9c6f9a2fb97d5b5fcf112ba28291a
tiaak <istianaasalsabila15@gmail.com>
2023-12-16 22:21:08 +0700

    implement camera and gallery intent

--- app/src/main/java/com/example/inswap/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/example/inswap/ui/scan/ScanFragment.kt
@@ -1,15 +1,20 @@
 package com.example.inswap.ui.scan
 
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.inswap.R
 import com.example.inswap.databinding.FragmentHomeBinding
 import com.example.inswap.databinding.FragmentScanBinding
+import com.example.inswap.getImageUri
 import com.example.inswap.ui.home.HomeViewModel
 
 class ScanFragment : Fragment() {
@@ -20,6 +25,7 @@ class ScanFragment : Fragment() {
 
     private var _binding: FragmentScanBinding? = null
     private val binding get() = _binding!!
+    private var currentImageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,20 +39,50 @@ class ScanFragment : Fragment() {
         _binding = FragmentScanBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textScan
-//        scanViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.cameraButton.setOnClickListener { startCamera() }
+
         return root
     }
 
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
+    }
+
+    private fun startCamera() {
+        currentImageUri = getImageUri(requireActivity())
+        launcherIntentCamera.launch(currentImageUri)
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            showImage()
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//        // TODO: Use the ViewModel
-//    }
+
 
 }
--- app/src/main/java/com/example/inswap/ui/upload/UploadActivity.kt
+++ app/src/main/java/com/example/inswap/ui/upload/UploadActivity.kt
@@ -1,13 +1,61 @@
 package com.example.inswap.ui.upload
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.inswap.R
+import com.example.inswap.databinding.ActivityUploadBinding
+import com.example.inswap.getImageUri
 
 class UploadActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUploadBinding
+    private var currentImageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.cameraButton.setOnClickListener { startCamera() }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
+    }
+
+    private fun startCamera() {
+        currentImageUri = getImageUri(this)
+        launcherIntentCamera.launch(currentImageUri)
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            showImage()
+        }
     }
 }
