e8680b89ed63f3345c779be08aa264f5cdcca74f
tiaak <istianaasalsabila15@gmail.com>
2023-12-21 21:44:46 +0700

    adding maggot hotline activity

---
+++ app/src/main/java/com/example/inswap/ui/maggot/MaggotActivity.kt
@@ -0,0 +1,12 @@
+package com.example.inswap.ui.maggot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.inswap.R
+
+class MaggotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maggot)
+    }
+}
